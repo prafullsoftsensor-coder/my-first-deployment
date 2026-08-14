ccd9678140a98395ad0ce11d716a3f2bb1c1b78f
Prafull Singh Rao <prafull.singh9235@softsensor.ai>
2026-08-14 11:24:44 +0530

    Enviornment

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
